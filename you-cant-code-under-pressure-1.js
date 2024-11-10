26595f26c999413965abc2355253115a64a50f5f
IrynaS1 <snowqueen-2007@mail.ru>
2024-11-10 11:09:16 +0300

    You Can-t Code Under Pressure #1

---
+++ you-cant-code-under-pressure-1.js
@@ -0,0 +1,11 @@
+/* Задача
+Пишите код как можно быстрее! Вам нужно удвоить целое число и вернуть его.
+
+*/
+
+function doubleInteger(i) {
+	console.log(i ** 2);
+	return i ** 2;
+}
+
+doubleInteger(2);
