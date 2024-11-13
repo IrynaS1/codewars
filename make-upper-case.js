2558d83b34a5e0e554efdd66c416305492afeab2
IrynaS1 <snowqueen-2007@mail.ru>
2024-11-13 09:01:02 +0300

    MakeUpperCase

---
+++ make-upper-case.js
@@ -0,0 +1,12 @@
+/* Задача
+Напишите функцию, которая преобразует входную строку в верхний регистр.
+
+*/
+
+function makeUpperCase(str) {
+	console.log(str.toUpperCase());
+	return str.toUpperCase();
+}
+
+
+makeUpperCase("Hello");
