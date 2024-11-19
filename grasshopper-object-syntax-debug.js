df3c5cab2e5fe2e2e1fc4046a0dcf25c3ec8168d
IrynaS1 <snowqueen-2007@mail.ru>
2024-11-19 11:23:41 +0300

    Grasshopper - Object syntax debug

---
+++ grasshopper-object-syntax-debug.js
@@ -0,0 +1,26 @@
+/* Задача
+Отладка объектов
+Создавая игру в стиле «Зорк», вы создаёте объект «комнаты». К сожалению, игра не работает. 
+Найдите все ошибки в объекте «комнаты», чтобы ваша игра снова заработала.
+
+*/
+
+var rooms = {
+	first: {
+		description: 'This is the first room',
+		items: {
+			chair: 'The old chair looks comfortable',
+			lamp: 'This lamp looks ancient',
+		},
+		second: {
+			description: 'This is the second room',
+			items: {
+				couch: 'This couch looks like it would hurt your back',
+				table: 'On the table there is an unopened bottle of water',
+			}
+		}
+	}
+}
+
+console.log(rooms.first.items.lamp);
+
