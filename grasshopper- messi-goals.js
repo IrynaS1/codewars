dee0608d0c00bc9510edb7f1b6f1e78570e91044
IrynaS1 <snowqueen-2007@mail.ru>
2024-11-19 16:33:33 +0300

    Grasshopper - Messi Goals

---
+++ grasshopper- messi-goals.js
@@ -0,0 +1,26 @@
+/* Задача
+Тотал голов Месси
+Используйте переменные, чтобы найти сумму голов, забитых Месси в 3 соревнованиях
+
+Информация
+Статистика забитых мячей Месси:
+
+Соревнование	Голы
+La Liga	43
+Лига чемпионов	10
+Copa del Rey	5
+Задача
+Создайте эти три переменные и сохраните соответствующие значения, используя приведенную выше таблицу:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Создайте четвёртую переменную с именем totalGoals для хранения суммы всех голов Месси в этом году.
+
+*/
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+console.log(totalGoals);
