e2f9e729e09587dcaab3b41b068854a15e7f4b6c
IrynaS1 <snowqueen-2007@mail.ru>
2024-11-20 17:23:29 +0300

    Returning Strings

---
+++ returning-strings.js
@@ -0,0 +1,13 @@
+/* Задача
+Создайте функцию, которая будет возвращать приветствие с использованием входных данных; 
+ваша программа должна возвращать "Hello, <name> how are you doing today?".
+
+[Убедитесь, что вы вводите именно то, что я написал, иначе программа может работать некорректно]
+*/
+
+function greet(name) {
+	console.log(`Hello, ${name} how aren you doing today?`);
+	return `Hello, ${name} how aren you doing today?`;
+ }
+
+greet("Ryan");
